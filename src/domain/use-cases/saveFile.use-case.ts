0d86ea924de8061efd4ffce8658bf31dd8c827b7
TrujiDev <eetgrisales@gmail.com>
2023-12-12 21:17:22 -0500

    Add SaveFileUseCase and SaveFile implementation

---
+++ src/domain/use-cases/saveFile.use-case.ts
@@ -0,0 +1,30 @@
+import fs from 'fs';
+
+export interface SaveFileUseCase {
+	execute: (options: Options) => boolean;
+}
+
+export interface Options {
+	fileContent: string;
+	destinationPath?: string;
+	fileName?: string;
+}
+
+export class SaveFile implements SaveFileUseCase {
+	constructor() {}
+
+	execute({
+		fileContent,
+		destinationPath = 'outputs',
+		fileName = 'table',
+	}: Options): boolean {
+		try {
+			fs.mkdirSync(destinationPath, { recursive: true });
+			fs.writeFileSync(`${destinationPath}/${fileName}.txt`, fileContent);
+			return true;
+		} catch (error) {
+			console.log(error);
+			return false;
+		}
+	}
+}
